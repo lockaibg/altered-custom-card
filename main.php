a1ddf189d99aa6bb40fbdfc60f8931e4b357fe4f
phile <e2869u@etu.univ-lorraine.fr>
2025-11-06 13:09:15 +0100

    finalysation des updates du formulaire

--- main.php
+++ main.php
@@ -7,66 +7,124 @@
     <link rel="stylesheet" href="../styles.css">    
     <script>
         window.onload = function () {
+            var type = document.getElementById("card-type").value;
+            const container = document.getElementById("additional-type");
+            //supprimer les éléments pour hero
+            if(type === "hero") {
+                document.getElementById("hand-cost-div").innerHTML = "";
+                document.getElementById("reserve-cost-div").innerHTML = "";
+                document.getElementById("card-bonus-div").innerHTML = "";
+            } else if (document.getElementById("hand-cost-div").innerHTML === ""){
+                document.getElementById("hand-cost-div").innerHTML = '<br/><label for="hand-cost">Hand cost :</label><input type="number" id="hand-cost" name="hand-cost" min="0" value="0" max="10"/><br/>';
+                document.getElementById("reserve-cost-div").innerHTML = '<br/><label for="reserve-cost">Reserve cost :</label><input type="number" id="reserve-cost" name="reserve-cost" min="0" value="0" max="10"/><br/>';
+                document.getElementById("card-bonus-div").innerHTML = '<br/><label for="card-bonus">Reserve effect :</label><textarea id="card-bonus" name="card-bonus"></textarea><br/>';
+            }
+            //vérification du type sélectionné au load de la page
+            fetch(`update.php?type=${type}`)
+                .then(response => response.text())
+                .then(html => {
+                    container.innerHTML = html;
+                    //si on a sélectionné un token on ajoute aussi un listener sur le type du token
+                        const token = container.querySelector("#token-type");
+                        if(token) {
+                            token.addEventListener("change", (e) => {
+                                const token_type = e.target.value;
+                                fetch(`token_type.php?type=${token_type}`)
+                                    .then(response => response.text())
+                                    .then(html => {
+                                        document.getElementById("additional-token").innerHTML = html;
+                                    });
+                            });
+                        }
+                });
+            //quand card-type est modifié on ajoute ou supprime le php correspondant au type sélectionné
             document.getElementById("card-type").addEventListener("change", (e) => {
-                var value = e.target.value;
-                switch(value) {
-                    case "token":
-                        fetch(`update.php?type[]=stats&type[]=token`)
-                        .then(response => response.text())
-                        .then(html => {
-                            document.getElementById("additional-type").innerHTML = html;
-                        });
-                        break;
-                    case "character":
-                        fetch(`update.php?type=stats`)
-                        .then(response => response.text())
-                        .then(html => {
-                            document.getElementById("additional-type").innerHTML = html;
-                        });
-                        break;
-                    default:
-                        document.getElementById("additional-type").innerHTML = "";
-                        break;
+                type = e.target.value;
+                //supprimer les éléments pour hero
+                if(type === "hero") {
+                    document.getElementById("hand-cost-div").innerHTML = "";
+                    document.getElementById("reserve-cost-div").innerHTML = "";
+                    document.getElementById("card-bonus-div").innerHTML = "";
+                } else if (document.getElementById("hand-cost-div").innerHTML === ""){
+                    document.getElementById("hand-cost-div").innerHTML = '<br/><label for="hand-cost">Hand cost :</label><input type="number" id="hand-cost" name="hand-cost" min="0" value="0" max="10"/><br/>';
+                    document.getElementById("reserve-cost-div").innerHTML = '<br/><label for="reserve-cost">Reserve cost :</label><input type="number" id="reserve-cost" name="reserve-cost" min="0" value="0" max="10"/><br/>';
+                    document.getElementById("card-bonus-div").innerHTML = '<br/><label for="card-bonus">Reserve effect :</label><textarea id="card-bonus" name="card-bonus"></textarea><br/>';
                 }
+                fetch(`update.php?type=${type}`)
+                    .then(response => response.text())
+                    .then(html => {
+                        container.innerHTML = html;
+                        //si on a sélectionné un token on ajoute aussi un listener sur le type du token
+                        const token = container.querySelector("#token-type");
+                        if(token) {
+                            token.addEventListener("change", (e) => {
+                                const token_type = e.target.value;
+                                fetch(`token_type.php?type=${token_type}`)
+                                    .then(response => response.text())
+                                    .then(html => {
+                                        document.getElementById("additional-token").innerHTML = html;
+                                    });
+                            });
+                        }
+                    });
             });
         }
+        
     </script>
 </head>
 <body>
-        <label for="name">Card name</label>
-        <input type="text" id="lognamein" name="name"/>
-        <br/><br/>   
-        <!--en js : modifier le formulaire pour ajouter des options ou non en fonction du type sélectionné-->
-        <label for="card-type">Type :</label>
-        <select id="card-type" name="card-type">
-            <option value="permanent">Permanent</option>
-            <option value="spell">Spell</option>
-            <option value="token">Token</option>
-            <option value="hero">Hero</option>
-            <option value="character">Character</option>
-        </select>
+    <label for="name">Card name</label>
+    <input type="text" id="name" name="name"/>
+    <br/><br/>   
+    <!--en js : modifier le formulaire pour ajouter des options ou non en fonction du type sélectionné-->
+    <label for="card-type">Type :</label>
+    <select id="card-type" name="card-type">
+        <option value="permanent">Permanent</option>
+        <option value="spell">Spell</option>
+        <option value="token">Token</option>
+        <option value="hero">Hero</option>
+        <option value="character">Character</option>
+    </select>
+    <br/>
+    <div id="additional-type"></div>
+    <br/>
+    <label for="card-background">Faction :</label>
+    <select id="card-background" name="card-background">
+        <option value="ordis">Ordis</option>
+        <option value="bravos">Bravos</option>
+        <option value="muna">Muna</option>
+        <option value="lyra">Lyra</option>
+        <option value="yzmir">Yzmir</option>
+        <option value="axiom">Axiom</option>
+    </select>
+    <br/><div id="hand-cost-div">
         <br/>
-        <div id="additional-type"></div>
-        <br/>
-        <label for="card-background">Faction :</label>
-        <select id="card-background" name="card-background">
-            <option value="ordis">Ordis</option>
-            <option value="bravos">Bravos</option>
-            <option value="muna">Muna</option>
-            <option value="lyra">Lyra</option>
-            <option value="yzmir">Yzmir</option>
-            <option value="axiom">Axiom</option>
-        </select>
-        <br/><br/>
         <label for="hand-cost">Hand cost :</label>
         <input type="number" id="hand-cost" name="hand-cost" min="0" value="0" max="10"/>
-        <br/><br/>
+        <br/>
+    </div>
+    <div id="reserve-cost-div">
+        <br/>
         <label for="reserve-cost">Reserve cost :</label>
         <input type="number" id="reserve-cost" name="reserve-cost" min="0" value="0" max="10"/>
-        <br/><br/>
-        <label for="card-image">Illustration :</label>
-        <input type="file" id="card-image" name="card-image" accept="image/*" />
-        <br/><br/>
+        <br/>
+    </div>
+    <br/>
+    <label for="card-image">Illustration :</label>
+    <input type="file" id="card-image" name="card-image" accept="image/*" />
+    <br/><br/>
+    <label for="card-effect">Effect :</label>
+    <textarea id="card-effect" name="card-effect"   ></textarea>
+    <br/><br/>
+    <label for="card-lore">Lore :</label>
+    <textarea id="card-lore" name="card-lore"></textarea>
+    <br/><div id="card-bonus-div">
+        <br/>
+        <label for="card-bonus">Reserve effect :</label>
+        <textarea id="card-bonus" name="card-bonus"></textarea>
+        <br/>
+    </div>
+    <br/>
     <form method="POST" action="saveFile.php" id="create-card-form">    
         <input type="submit" value="Save">
     </form>
--- update.php
+++ update.php
@@ -1,79 +1,183 @@
 <?php
-    if(isset($_GET["type"])) {
-        if(is_array($_GET["type"])) {
-            foreach($_GET["type"] as $param) {
-                switch($param) {
-                    case "stats":
-                        ?>
-                            <br/>
-                            <label for="hand-cost">Hand cost</label>
-                            <input type="radio" name="hand-cost" value="0" checked />
-                            <label for="0">0</label>
-                            <input type="radio" name="hand-cost" value="1"/>
-                            <label for="1">1</label>
-                            <input type="radio" name="hand-cost" value="2"/>
-                            <label for="2">2</label>
-                            <input type="radio" name="hand-cost" value="3"/>
-                            <label for="3">3</label>
-                            <input type="radio" name="hand-cost" value="4"/>
-                            <label for="4">4</label>
-                            <input type="radio" name="hand-cost" value="5"/>
-                            <label for="5">5</label>
-                            <input type="radio" name="hand-cost" value="6"/>
-                            <label for="6">6</label>
-                            <input type="radio" name="hand-cost" value="7"/>
-                            <label for="7">7</label>
-                            <input type="radio" name="hand-cost" value="8"/>
-                            <label for="8">8</label>
-                            <input type="radio" name="hand-cost" value="9"/>
-                            <label for="9">9</label>
-                            <input type="radio" name="hand-cost" value="10"/>
-                            <label for="10">10</label>
-                            <br/>
-                        <?php
-                        break;
-                    case "token":
-                        ?>
-                            <br/>
-                            <label for="token-type">Token type :</label>
-                            <select name="token-type" id="token-type">
-                                <option value="character">Character</option>
-                                <option value="landmark">Landmark</option>
-                            </select>
-                            <br/>
-                        <?php
-                        break;
-                } 
-            }
-        } else {
-            switch($_GET["type"]) {
-                case "stats":
-                    ?><br/>
-                        <input type="radio" name="hand-cost" value="0" checked />
-                        <label for="0">0</label>
-                        <input type="radio" name="hand-cost" value="1"/>
-                        <label for="1">1</label>
-                        <input type="radio" name="hand-cost" value="2"/>
-                        <label for="2">2</label>
-                        <input type="radio" name="hand-cost" value="3"/>
-                        <label for="3">3</label>
-                        <input type="radio" name="hand-cost" value="4"/>
-                        <label for="4">4</label>
-                        <input type="radio" name="hand-cost" value="5"/>
-                        <label for="5">5</label>
-                        <input type="radio" name="hand-cost" value="6"/>
-                        <label for="6">6</label>
-                        <input type="radio" name="hand-cost" value="7"/>
-                        <label for="7">7</label>
-                        <input type="radio" name="hand-cost" value="8"/>
-                        <label for="8">8</label>
-                        <input type="radio" name="hand-cost" value="9"/>
-                        <label for="9">9</label>
-                        <input type="radio" name="hand-cost" value="10"/>
-                        <label for="10">10</label>
-                    <br/><?php
-                    break;
-            }         
-        }
+    if(isset($_GET["type"])) {  
+        switch($_GET["type"]) {
+        case "character":
+            ?>
+                <br/>
+                <label for="earth">Earth</label>
+                <input type="radio" name="earth" value="0" checked />
+                <label for="0">0</label>
+                <input type="radio" name="earth" value="1"/>
+                <label for="1">1</label>
+                <input type="radio" name="earth" value="2"/>
+                <label for="2">2</label>
+                <input type="radio" name="earth" value="3"/>
+                <label for="3">3</label>
+                <input type="radio" name="earth" value="4"/>
+                <label for="4">4</label>
+                <input type="radio" name="earth" value="5"/>
+                <label for="5">5</label>
+                <input type="radio" name="earth" value="6"/>
+                <label for="6">6</label>
+                <input type="radio" name="earth" value="7"/>
+                <label for="7">7</label>
+                <input type="radio" name="earth" value="8"/>
+                <label for="8">8</label>
+                <input type="radio" name="earth" value="9"/>
+                <label for="9">9</label>
+                <input type="radio" name="earth" value="10"/>
+                <label for="10">10</label>
+                <br/>
+                <br/>
+                <label for="leaf">Leaf</label>
+                <input type="radio" name="leaf" value="0" checked />
+                <label for="0">0</label>
+                <input type="radio" name="leaf" value="1"/>
+                <label for="1">1</label>
+                <input type="radio" name="leaf" value="2"/>
+                <label for="2">2</label>
+                <input type="radio" name="leaf" value="3"/>
+                <label for="3">3</label>
+                <input type="radio" name="leaf" value="4"/>
+                <label for="4">4</label>
+                <input type="radio" name="leaf" value="5"/>
+                <label for="5">5</label>
+                <input type="radio" name="leaf" value="6"/>
+                <label for="6">6</label>
+                <input type="radio" name="leaf" value="7"/>
+                <label for="7">7</label>
+                <input type="radio" name="leaf" value="8"/>
+                <label for="8">8</label>
+                <input type="radio" name="leaf" value="9"/>
+                <label for="9">9</label>
+                <input type="radio" name="leaf" value="10"/>
+                <label for="10">10</label>
+                <br/>
+                <br/>
+                <label for="ocean">Ocean</label>
+                <input type="radio" name="ocean" value="0" checked />
+                <label for="0">0</label>
+                <input type="radio" name="ocean" value="1"/>
+                <label for="1">1</label>
+                <input type="radio" name="ocean" value="2"/>
+                <label for="2">2</label>
+                <input type="radio" name="ocean" value="3"/>
+                <label for="3">3</label>
+                <input type="radio" name="ocean" value="4"/>
+                <label for="4">4</label>
+                <input type="radio" name="ocean" value="5"/>
+                <label for="5">5</label>
+                <input type="radio" name="ocean" value="6"/>
+                <label for="6">6</label>
+                <input type="radio" name="ocean" value="7"/>
+                <label for="7">7</label>
+                <input type="radio" name="ocean" value="8"/>
+                <label for="8">8</label>
+                <input type="radio" name="ocean" value="9"/>
+                <label for="9">9</label>
+                <input type="radio" name="ocean" value="10"/>
+                <label for="10">10</label>
+                <br/>
+            <?php
+            break;
+        case "token":
+            ?><div id="additional-token">
+            <br/>
+            
+                <label for="earth">Earth</label>
+                <input type="radio" name="earth" value="0" checked />
+                <label for="0">0</label>
+                <input type="radio" name="earth" value="1"/>
+                <label for="1">1</label>
+                <input type="radio" name="earth" value="2"/>
+                <label for="2">2</label>
+                <input type="radio" name="earth" value="3"/>
+                <label for="3">3</label>
+                <input type="radio" name="earth" value="4"/>
+                <label for="4">4</label>
+                <input type="radio" name="earth" value="5"/>
+                <label for="5">5</label>
+                <input type="radio" name="earth" value="6"/>
+                <label for="6">6</label>
+                <input type="radio" name="earth" value="7"/>
+                <label for="7">7</label>
+                <input type="radio" name="earth" value="8"/>
+                <label for="8">8</label>
+                <input type="radio" name="earth" value="9"/>
+                <label for="9">9</label>
+                <input type="radio" name="earth" value="10"/>
+                <label for="10">10</label>
+                <br/>
+                <br/>
+                <label for="leaf">Leaf</label>
+                <input type="radio" name="leaf" value="0" checked />
+                <label for="0">0</label>
+                <input type="radio" name="leaf" value="1"/>
+                <label for="1">1</label>
+                <input type="radio" name="leaf" value="2"/>
+                <label for="2">2</label>
+                <input type="radio" name="leaf" value="3"/>
+                <label for="3">3</label>
+                <input type="radio" name="leaf" value="4"/>
+                <label for="4">4</label>
+                <input type="radio" name="leaf" value="5"/>
+                <label for="5">5</label>
+                <input type="radio" name="leaf" value="6"/>
+                <label for="6">6</label>
+                <input type="radio" name="leaf" value="7"/>
+                <label for="7">7</label>
+                <input type="radio" name="leaf" value="8"/>
+                <label for="8">8</label>
+                <input type="radio" name="leaf" value="9"/>
+                <label for="9">9</label>
+                <input type="radio" name="leaf" value="10"/>
+                <label for="10">10</label>
+                <br/>
+                <br/>
+                <label for="ocean">Ocean</label>
+                <input type="radio" name="ocean" value="0" checked />
+                <label for="0">0</label>
+                <input type="radio" name="ocean" value="1"/>
+                <label for="1">1</label>
+                <input type="radio" name="ocean" value="2"/>
+                <label for="2">2</label>
+                <input type="radio" name="ocean" value="3"/>
+                <label for="3">3</label>
+                <input type="radio" name="ocean" value="4"/>
+                <label for="4">4</label>
+                <input type="radio" name="ocean" value="5"/>
+                <label for="5">5</label>
+                <input type="radio" name="ocean" value="6"/>
+                <label for="6">6</label>
+                <input type="radio" name="ocean" value="7"/>
+                <label for="7">7</label>
+                <input type="radio" name="ocean" value="8"/>
+                <label for="8">8</label>
+                <input type="radio" name="ocean" value="9"/>
+                <label for="9">9</label>
+                <input type="radio" name="ocean" value="10"/>
+                <label for="10">10</label>
+            </div>
+                <br/>
+                <label for="token-type">Token type :</label>
+                <select name="token-type" id="token-type">
+                    <option value="character">Character</option>
+                    <option value="landmark">Landmark</option>
+                </select>
+                <br/>
+            <?php
+            break;
+        case "hero":
+            ?>
+                <br/>
+                <label for="hero-reserve">Reserve :</label>
+                <input type="number" id="hero-reserve" name="hero-reserve" min="0" value="0" max="5"/>
+                <br/><br/>
+                <label for="hero-landmark">Landmarks :</label>
+                <input type="number" id="hero-landmark" name="hero-landmark" min="0" value="0" max="5"/>
+                <br/> 
+            <?php
+            break;
+        }         
     }
 ?>
